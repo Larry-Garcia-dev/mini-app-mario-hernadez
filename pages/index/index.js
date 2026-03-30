c090491823386e7a683e07266197444551ab5671
Albeiro Chunza <albeirochunza90@gmail.com>
2026-03-30 12:44:08 -0500

    adjust index and added animation on start

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,7 @@ Page({
   data: {
     brandName: 'MARIO HERNÁNDEZ',
     isMenuOpen: true,
+    menuAnimated: false,
     heroImageSrc: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Mario_Hernandez_Mini_APP-OEOPmwvUYgkHsgxOqtZwmHgfiAGNrp.png',
     menuItems: [
       { 
@@ -28,12 +29,32 @@ Page({
         id: 5, 
         label: 'Outlet', 
         route: '/pages/category/index?type=outlet' 
+      },
+      { 
+        id: 6, 
+        label: 'Outlet', 
+        route: '/pages/category/index?type=outlet' 
+      },
+      { 
+        id: 7, 
+        label: 'Outlet', 
+        route: '/pages/category/index?type=outlet' 
+      },
+      { 
+        id: 8, 
+        label: 'Outlet', 
+        route: '/pages/category/index?type=outlet' 
       }
     ]
   },
 
   onLoad() {
-    console.log('Index page loaded');
+    // Iniciar animacion del menu despues de un pequeno delay
+    setTimeout(() => {
+      this.setData({
+        menuAnimated: true
+      });
+    }, 100);
   },
 
   handleToggleMenu() {
